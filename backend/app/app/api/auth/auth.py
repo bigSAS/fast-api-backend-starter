2cbc39326c7b6a927630c607f97a2b12850494df
bigSAS <sas.it.tomasz.majk@gmail.com>
2021-06-13 16:25:07 +0200

    update auth

--- backend/app/app/api/auth/auth.py
+++ backend/app/app/api/auth/auth.py
@@ -1,11 +1,12 @@
 from passlib.context import CryptContext
-from jose import JWTError, jwt
+from jose import jwt
 from fastapi import Depends
 from sqlalchemy.orm import Session
 from datetime import datetime, timedelta
 from typing import Optional
 from app.crud import crud_user
 from app.errors.api import AuthError
+from app.models.user import User
 
 SECRET_KEY = "e41fae79f843957edfc3d3221bc58af4cf3d03a48c77e86f5d02c7f807f8194b"  # todo: config / env var?
 ALGORITHM = "HS256"
@@ -23,19 +24,19 @@ def get_password_hash(password: str) -> str:
     return pwd_context.hash(password)
 
 
-def authenticate_user(username: str, password: str, db: Session = Depends()):
+def authenticate_user(username: str, password: str, db: Session = Depends()) -> User:
     user = crud_user.get_user_by_username(db, username)
     if not user: raise AuthError('Invalid username or password')
     if not _is_password_valid(password, user.hashed_password): raise AuthError('Invalid username or password')
     return user
 
 
-def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None) -> str:
     to_encode = data.copy()
     if expires_delta:
         expire = datetime.now() + expires_delta
     else:
         expire = datetime.now() + timedelta(minutes=15)
     to_encode.update({"exp": expire})
-    enconded_jwt = jwt.encode(to_encode, key=SECRET_KEY, algorithm=ALGORITHM)
-    return enconded_jwt
+    encoded_jwt = jwt.encode(to_encode, key=SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
