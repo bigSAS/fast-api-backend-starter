a75a481fd0eb7ecd9b11e1addb915019944a973c
bigSAS <sas.it.tomasz.majk@gmail.com>
2021-06-23 19:08:18 +0200

    add 2do

--- backend/app/app/api/auth/auth.py
+++ backend/app/app/api/auth/auth.py
@@ -40,3 +40,5 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None) -
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, key=SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
+
+# todo: refresh token
