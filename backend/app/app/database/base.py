0db5de3fb5cfe58d49db35444a4625401352aaf8
bigSAS <sas.it.tomasz.majk@gmail.com>
2021-06-23 19:12:41 +0200

    cleanup

--- backend/app/app/database/base.py
+++ backend/app/app/database/base.py
@@ -1,3 +1,6 @@
+"""
+# todo: check if needed
+"""
 from app.database.setup import Base
 from app.database.models.user import User
 from app.database.models.note import Note
--- backend/app/app/repositories/readme.md
+++
@@ -1,1 +0,0 @@
-# Repository jus like in last flask app
