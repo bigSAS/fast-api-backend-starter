be334bdaa2e4dcee4f2a0e8b19c55690d590708e
bigSAS <sas.it.tomasz.majk@gmail.com>
2021-07-10 00:17:38 +0200

    add admin permission to default admin user

--- backend/app/app/scripts/init_admin.py
+++ backend/app/app/scripts/init_admin.py
@@ -1,6 +1,8 @@
 from app.api.auth import auth
+from app.database.models.permission import Permission as PermissionEntity
 from app.database.models.user import User as UserEntity
 from app.database.setup import SessionLocal
+from app.repositories.permissions import PermissionsRepository
 from app.repositories.users import UserRepository
 
 
@@ -16,6 +18,11 @@ def init_admin():
             hashed_password=auth.get_password_hash('admin')
         )
         user_repository.save(default_admin)
+        admin_permission = PermissionEntity(
+            name="IS_ADMIN",
+            user_id=default_admin.id
+        )
+        PermissionsRepository(db).save(admin_permission)
         print("Done.")
     else:
         print("Skipping adding admin user")
